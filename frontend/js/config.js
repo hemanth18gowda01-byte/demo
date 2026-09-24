7378ec84e31879129781138a079557ba4bf45010
sampreetech <workuniverse0369@gmail.com>
2026-09-24 15:48:35 +0530

    Restore deployed contract address configuration

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,10 +1,10 @@
 export const CONTRACTS = {
     smartAccount: "0x29d7F532A8a271cBaf1f27f2312Bf2188612e929",
-    identity: "0x29d7F532A8a271cBaf1f27f2312Bf2188612e929",
-    accessControl: "0x29d7F532A8a271cBaf1f27f2312Bf2188612e929",
-    assetNFT: "0x29d7F532A8a271cBaf1f27f2312Bf2188612e929",
-    stableCoin: "0x29d7F532A8a271cBaf1f27f2312Bf2188612e929",
-    marketplace: "0x29d7F532A8a271cBaf1f27f2312Bf2188612e929"
+    identity: "0xCbf2f3fEd1Af81c816F1C6B5219d0779bd50e758",
+    accessControl: "0x463C2C1f72a5593615881B0040c2F80fB4815c3f",
+    assetNFT: "0xfb8c47c3bE943601d23ECae90194adb22752C16d",
+    stableCoin: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
+    marketplace: "0x08233BcBaBf0a67Ba0cCe00494c35cF8eC943f38"
 };
 
 export const SEPOLIA_CHAIN_ID = "0xaa36a7";
