3e66a12d7b54e74a5ee201ae5a2123831855bc2b
Vaishnavi <vaishnavigowda0805@gmail.com>
2026-09-26 12:53:48 +0530

    Point frontend to deployed marketplace

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,7 +4,7 @@ export const CONTRACTS = {
     accessControl: "0x6411563A485D5DDAcEFB85ae18Ae1D37E8363BD3",
     assetNFT: "0x44Cbbf18eCcA63522e29f20Ec223915CDf429a16",
     stableCoin: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
-    marketplace: "0x0000000000000000000000000000000000000000"
+    marketplace: "0x6Ffb5723Fe8E52F4Fb27202678A1638a22BEd4Db"
 };
 
 // This wallet owns the SmartAccount and is the intended owner of every ownable application contract.
